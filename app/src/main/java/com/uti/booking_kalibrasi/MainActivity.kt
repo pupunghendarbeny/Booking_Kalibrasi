52b059122b55760e56143563ac24b654dcf246e6
kiatriosaputra <kia_trio_saputra@teknokrat.ac.id>
2025-06-26 18:50:10 +0700

    membuat tampilan untuk berpindah ke tampilan login dengan durasi 2 detik

--- app/src/main/java/com/uti/booking_kalibrasi/MainActivity.kt
+++ app/src/main/java/com/uti/booking_kalibrasi/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.uti.booking_kalibrasi
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 2000 // 2 seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Navigate to login after splash
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
     }
 }
