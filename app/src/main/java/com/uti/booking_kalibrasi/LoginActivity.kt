d0a4945ae49f207f57bd9386288e0e4992a82abb
pupunghendarbeny <pupung.hb@gmail.com>
2025-06-26 14:34:53 +0700

    membuat tampilan username password dan login dan menyesuaikan agar mudah di pahami

---
+++ app/src/main/java/com/uti/booking_kalibrasi/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.uti.booking_kalibrasi
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
