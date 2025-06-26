f3f0ac3efd0334a9f28e93d4d622f71917a933e2
kiatriosaputra <kia_trio_saputra@teknokrat.ac.id>
2025-06-26 16:49:14 +0700

    Membuat ServiceListActivity.kt

---
+++ app/src/main/java/com/uti/booking_kalibrasi/ServiceListActivity.kt
@@ -0,0 +1,10 @@
+package com.uti.booking_kalibrasi
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+
