51e3a419ca500d5aa73c43f0ed43809dace6db31
kholis442 <kholis.ogee@gmail.com>
2025-06-26 20:42:27 +0700

    membuat class baru ServiceAdapter.kt

---
+++ app/src/main/java/com/uti/booking_kalibrasi/ServiceAdapter.kt
@@ -0,0 +1,16 @@
+package com.uti.booking_kalibrasi
+
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.text.NumberFormat
+import java.util.*
+
